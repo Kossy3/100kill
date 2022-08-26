8ce8d7a61cf5202e1981d3ec2034dd1a2695e9c4
Kossy3 <ykoshi873@gmail.com>
2022-08-26 19:27:10 +0900

    MusicPlayerを追加

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AudioSynthesis.Synthesis;
 
 public class MusicPlayer : MonoBehaviour
 {
+    Synthesizer synth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        synth = new Synthesizer(8000, 1).NoteOn((System.Int32)9, (System.Int32)70, (System.Int32)100);
     }
 
     // Update is called once per frame
