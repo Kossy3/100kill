897127133bb22a02b669041cfaef76e93f81d22d
Kossy3 <ykoshi873@gmail.com>
2022-09-07 00:35:22 +0900

    テスト用スクリプトのコメントアウト

--- Assets/Scripts/MusicGenerator.cs
+++ Assets/Scripts/MusicGenerator.cs
@@ -11,7 +11,7 @@ public class MusicGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        test_start();
+        //test_start();
     }
 
     void test_start(){
