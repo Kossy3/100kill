840b0ae73ce27610570c96ee4bc7a9cf9efc6801
Kossy3 <ykoshi873@gmail.com>
2022-09-06 16:18:13 +0900

    メソッドの仮実装とテスト用スクリプトの追加

--- Assets/Scripts/MusicGenerator.cs
+++ Assets/Scripts/MusicGenerator.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MusicGenerator : MonoBehaviour
 {
+#if UNITY_EDITOR
+    RythmGenerator rythmGenerator;
+    MusicPlayer musicPlayer;
+#endif
     // Start is called before the first frame update
     void Start()
     {
-        
+#if UNITY_EDITOR
+        test_start();
+#endif
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void test_start(){
+        musicPlayer = GameObject.Find("MusicPlayer").GetComponent<MusicPlayer>();
+        rythmGenerator = GameObject.Find("RhythmGenerator").GetComponent<RythmGenerator>();
+        var rhythm = rythmGenerator.generate_8bar_rhythm();
+        generate_8bar_music(rhythm);
+        musicPlayer.play_music(rhythm);
+    }
+
+    int[] generate_8bar_music(int[] rhythm){
+        return rhythm;
     }
 }
