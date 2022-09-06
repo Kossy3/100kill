b538ace51baf691b10a7d07d0b7d5fd3c0b59c8a
Kossy3 <ykoshi873@gmail.com>
2022-09-07 00:30:06 +0900

    MusicGeneratorの出力形式を変更

--- Assets/Scripts/MusicGenerator.cs
+++ Assets/Scripts/MusicGenerator.cs
@@ -11,20 +11,51 @@ public class MusicGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-#if UNITY_EDITOR
         test_start();
-#endif
     }
 
     void test_start(){
         musicPlayer = GameObject.Find("MusicPlayer").GetComponent<MusicPlayer>();
         rythmGenerator = GameObject.Find("RhythmGenerator").GetComponent<RythmGenerator>();
         var rhythm = rythmGenerator.generate_8bar_rhythm();
-        generate_8bar_music(rhythm);
-        musicPlayer.play_music(rhythm);
+        List<List<Note>> score = generate_8bar_music(rhythm);
+        StartCoroutine(musicPlayer.play_music_c(score));
     }
 
-    int[] generate_8bar_music(int[] rhythm){
-        return rhythm;
+    List<List<Note>> generate_8bar_music(int[] rhythm){
+        List<List<Note>> score = new List<List<Note>>();
+        //基本トラック
+        score.Add(generate_trak0(rhythm));
+        return score;
+    }
+
+    List<Note> generate_trak0(int[] rhythm){
+        List<Note> track = new List<Note>();
+        for (var i=0; i<rhythm.Length; i++){
+            if (i % 4 == 2){
+                track.Add(new Note(9, 38, 1f/2f, 1f/4f, 127));
+            }else{
+                track.Add(new Note(9, 38, 1f/2f, 1f/4f, 80));
+            }
+            if (rhythm[i] > 0){
+                track.Add(new Note(9, 57, 0f, 1f/4f, 127));
+            }        
+        }
+        return track;
+    }
+}
+
+public class Note {
+    public byte ch;
+    public byte no;
+    public float delta;
+    public float len;
+    public byte velocity;
+    public Note(byte ch, byte no, float delta, float len,byte velocity){
+        this.ch = ch;
+        this.no = no;
+        this.delta = delta;
+        this.len = len;
+        this.velocity = velocity;
     }
 }
